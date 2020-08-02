ae6c3c171a7133fa34e1a64e70f682e76c027229
Gavin Lu <gavin@shoplineapp.com>
2020-08-02 18:05:09 +0800

    deploy through capistrano test

--- deploy/capistrano/config/deploy.rb
+++ deploy/capistrano/config/deploy.rb
@@ -39,4 +39,4 @@ set :user, 'ubuntu'
 
 # Uncomment the following to require manually verifying the host key before first deploy.
 # set :ssh_options, verify_host_key: :secure
-set :ssh_options, { forward_agent: true, user: fetch(:user), keys: %w(~/.ssh/gogolook/job.pem) }
+#set :ssh_options, { forward_agent: true, user: fetch(:user), keys: %w(~/.ssh/gogolook/job.pem) }
