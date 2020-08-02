2e1cfa48a8ba9ae3b8b3d21f81b8d539acb475e4
Gavin Lu <gavin@shoplineapp.com>
2020-08-02 12:20:23 +0800

    commit home page

--- autoapp.py
+++ autoapp.py
@@ -8,3 +8,11 @@ from conduit.settings import DevConfig, ProdConfig
 CONFIG = DevConfig if get_debug_flag() else ProdConfig
 
 app = create_app(CONFIG)
+
+# home page
+from flask import Flask
+app = Flask(__name__)
+
+@app.route('/')
+def hello_world():
+    return 'Hello, World! This is Wei-Chyang Test'
