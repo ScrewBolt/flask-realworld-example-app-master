1abb9d87413a38e3597764ad43793a3152ff20d8
Gavin Lu <gavin@shoplineapp.com>
2020-08-02 18:06:46 +0800

    capistrano deploy current branch

--- deploy/capistrano/config/deploy.rb
+++ deploy/capistrano/config/deploy.rb
@@ -6,6 +6,7 @@ set :repo_url, "git@github.com:ScrewBolt/flask-realworld-example-app-master.git"
 
 # Default branch is :master
 # ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
+ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
 
 # Default deploy_to directory is /var/www/my_app_name
 # set :deploy_to, "/var/www/my_app_name"
