ed33bcefe9b8fef7787eb798f0494d578f09f919
Gavin Lu <gavin@shoplineapp.com>
2020-08-03 15:10:39 +0800

    add db migration

--- deploy/capistrano/config/deploy.rb
+++ deploy/capistrano/config/deploy.rb
@@ -40,4 +40,3 @@ set :user, 'ubuntu'
 
 # Uncomment the following to require manually verifying the host key before first deploy.
 # set :ssh_options, verify_host_key: :secure
-#set :ssh_options, { forward_agent: true, user: fetch(:user), keys: %w(~/.ssh/gogolook/job.pem) }
