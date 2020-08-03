3005209fe52b1d3ce27d4241fd6f4b895bd5e2bc
Gavin Lu <gavin@shoplineapp.com>
2020-08-03 15:05:30 +0800

    1. adjust comments 2. remove duplicate app on autoapp.py

--- autoapp.py
+++ autoapp.py
@@ -4,14 +4,10 @@ from flask.helpers import get_debug_flag
 
 from conduit.app import create_app
 from conduit.settings import DevConfig, ProdConfig
-
+from flask import Flask
 CONFIG = DevConfig if get_debug_flag() else ProdConfig
 
 app = create_app(CONFIG)
-
-# home page
-from flask import Flask
-app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
